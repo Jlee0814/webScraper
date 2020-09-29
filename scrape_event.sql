f119cb1129966a6724ac0a8a2d38e17254a451fc
Jlee0814 <49137753+Jlee0814@users.noreply.github.com>
2020-09-29 01:35:03 -0500

    Add files via upload

---
+++ scrape_event.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: scrape
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `event`
+--
+
+LOCK TABLES `event` WRITE;
+/*!40000 ALTER TABLE `event` DISABLE KEYS */;
+INSERT INTO `event` VALUES (85,'2020-09-28 00:00:00','Taipei, Taiwan','Computex Taipei','computerworld'),(86,'2020-09-30 00:00:00','New Orleans, LA','MSPWorld 2020 (Online only)','computerworld'),(87,'2020-10-01 00:00:00','Amsterdam, NL','TNW Conference','computerworld'),(88,'2020-10-05 00:00:00','Washington, DC','Voice Summit 2020','computerworld'),(89,'2020-10-06 00:00:00','Rancho Palos Verdes, CA','Virtual CIO 100/AGENDA20 Symposium','computerworld'),(90,'2020-10-06 00:00:00','La Jolla, CA','FiRe 2020','computerworld'),(91,'2020-10-07 00:00:00','San Francisco, CA','Slack Frontiers 2020 (Online only)','computerworld'),(92,'2020-10-19 00:00:00','Miami Beach, FL','Acronis Global Cyber Summit','computerworld'),(93,'2020-10-19 00:00:00','Online, Virtual','Gartner IT Symposium/Xpo Americas (Online only)','computerworld'),(94,'2020-10-26 00:00:00','San Diego, CA','Spinnaker Summit (Online Only)','computerworld'),(95,'2020-10-27 00:00:00','San Jose, CA','API World 2020','computerworld'),(96,'2020-10-27 00:00:00','Online, Virtual','Gartner IT Symposium/Xpo APAC (Online only)','computerworld'),(97,'2020-10-28 00:00:00','Los Angeles, CA','MWC Americas','computerworld'),(98,'2020-10-28 00:00:00','Berlin, Germany','GOTO Berlin 2020','computerworld'),(99,'2020-11-04 00:00:00','Santa Clara, CA','Blockchain Expo','computerworld'),(100,'2020-11-09 00:00:00','San Francisco, CA','Dreamforce','computerworld'),(101,'2020-11-09 00:00:00','Copenhagen, Denmark','GOTO Copenhagen 2020','computerworld'),(102,'2020-11-09 00:00:00','Las Vegas, NV','DevOps Enterprise Summit Las Vegas (Online only)','computerworld'),(103,'2020-11-09 00:00:00','Online, Virtual','Gartner IT Symposium/Xpo EMEA (Online only)','computerworld'),(104,'2020-11-15 00:00:00','Boston, MA','Splash 2020','computerworld'),(105,'2020-11-30 00:00:00','Las Vegas, NV','AWS re:Invent (Online only)','computerworld'),(106,'2020-12-07 00:00:00','Scottsdale, AZ','CSO 50','computerworld'),(107,'2020-12-07 00:00:00','Amsterdam, Netherlands','GOTO Amsterdam 2020','computerworld'),(108,'2021-03-23 00:00:00','Las Vegas, NV','SharePoint Conference 2021','computerworld');
+/*!40000 ALTER TABLE `event` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-09-29  0:53:26
